f060d8fe442d0f8950b208562f6487a7809ccfab
Pavan Kumar <pavancsms@gmail.com>
2016-07-22 20:11:22 -0500

    Update s3_config.php

--- s3_config.php
+++ s3_config.php
@@ -4,8 +4,8 @@ $bucket="Ace";
 if (!class_exists('S3'))require_once('S3.php');
 
 //AWS access info
-if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJ6X2GNOE7PNOGYFA');
-if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+if (!defined('awsAccessKey')) define('awsAccessKey', 'xxxxxxxxxxxxxxxxxx');
+if (!defined('awsSecretKey')) define('awsSecretKey', 'xxxxxxxxxxxxxx');
 
 $s3 = new S3(awsAccessKey, awsSecretKey);
 $s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
